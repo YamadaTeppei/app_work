c572fe9d3d84d3da40f2ee52042fbbf105e7ba64
yamada teppei <yamadateppei@yamadanoMacBook-Pro.local>
2020-01-09 22:48:42 +0900

    Added user's name in comment

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request,redirect, session, url_for
 from init_db import app, get_db, modify_db, query_db
 app = Flask(__name__)
-
+# topページ
 @app.route("/")
 def top():
     titles = query_db("title", "SELECT * FROM title")
@@ -9,7 +9,7 @@ def top():
         return render_template("index.html", titles=titles)
     return render_template("index.html",user="online", titles=titles)
 
-
+# ユーザー新規登録ページ
 @app.route("/register/",methods=["GET", "POST"])
 def register():
     if "userid" in session:
@@ -31,14 +31,14 @@ def register():
         user = query_db("user", "SELECT * FROM user WHERE email = ?", (session["useremail"],), True)
         session["userid"] = user["id"]
         return redirect(url_for("profile", userid=session["userid"]))
-
+# ユーザー一覧ページ
 @app.route("/view")
 def view():
     if "userid" not in session:
         return redirect(url_for("login"))
     users = query_db("user", "SELECT * FROM user")
     return render_template("view.html", users=users)
-
+# ログインページ
 @app.route("/login",methods=["GET", "POST"])
 def login():
     if "userid" in session:
@@ -60,23 +60,22 @@ def login():
         session["userid"] = user["id"]
 
         return redirect(url_for("profile", userid=session["userid"]))
-
+# ログアウト機能
 @app.route("/logout")
 def logout():
     session.clear()
     return redirect(url_for("top"))
-
+# プロフィールページ
 @app.route("/<int:userid>/")
 def profile(userid):
     if "userid" not in session:
         return redirect(url_for("login"))
-
     user = query_db("user", "SELECT * FROM user WHERE id = ?", (userid,), True)
     if int(userid) != session["userid"]:
         return render_template("Profile.html", user=user, f=0)
     else:
         return render_template("Profile.html", user=user, f=1)
-
+# プロフィール変更ページ
 @app.route("/<int:userid>/update", methods=["GET", "POST"])
 def update(userid):
     if "userid" not in session:
@@ -101,7 +100,7 @@ def update(userid):
         elif request.method == "GET":
             user = query_db("user", "SELECT * FROM user WHERE id = ?", (userid,), True)
             return render_template("Update.html", user=user)
-
+# タイトル新規作成ページ
 @app.route("/<int:userid>/newtitle", methods=["GET", "POST"])
 def newtitle(userid):
     if "userid" not in session:
@@ -112,7 +111,7 @@ def newtitle(userid):
         if request.method == "POST":
             modify_db("title", "INSERT INTO title  (name, user_id) VALUES(?, ?)", (request.form.get("title"), session['userid']))
             return redirect(url_for("top"))
-
+# タイトルページ
 @app.route("/<int:title_id>/title", methods=["GET", "POST"])
 def title(title_id):
     if "userid" in session:
@@ -120,7 +119,7 @@ def title(title_id):
     if request.method == "GET":
         comments = query_db("comment", "SELECT * FROM comment WHERE title_id = ?", (title_id,))
         title = query_db("title", "SELECT * FROM title WHERE id = ?", (title_id,), True)
-        return render_template("title.html",title=title, comments=comments)
+        return render_template("title.html",title=title, comments=comments, query_db=query_db, enumerate=enumerate)
     else:
         if "userid" not in session:
             return redirect(url_for("login"))
